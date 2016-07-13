a1c542a6abb0ce97e3ec82a8c7ba2d7d04a89775
Pierre-Emmanuel Mercier <Pierre-Emmanuel Mercier>
2016-07-13 08:42:57 +0200

    navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
+// Shared components
+import {NavBarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
+
+// Precompiled components
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    templateUrl: './app/app.component.html',
-    directives: [ROUTER_DIRECTIVES],
-    precompile: [HomeComponent, AboutComponent]
+    templateUrl: 'app.component.html',
+    directives: [ROUTER_DIRECTIVES, NavBarComponent, FooterComponent],
+    precompile :[HomeComponent,AboutComponent]
 })
 
 export class AppComponent {
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    selector: 'bs-navbar',
+    templateUrl: 'navbar.component.html',
+    directives : [ROUTER_DIRECTIVES]
+})
+export class NavBarComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
